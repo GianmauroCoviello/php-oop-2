449f5f343c15e3c518ee76ec6018786c34ac276f
GianmauroCoviello <user.mail like: "gianmaurocoviello@gmail.com>
2023-07-11 04:06:58 +0200

    [DOCS] impostata struttura layout prodotti

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+    require_once __DIR__ .'/models/dogs.php';
+    require_once __DIR__ .'/models/cats.php';
+    require_once __DIR__ .'/models/birds.php';
+
+
 
 ?>
 
@@ -8,9 +13,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <div class="row">
+            <h1>BOOLSHOP</h1>
+        </div>
+    </header>
+    <main>
+        
+        <div class="row">
+            <h2>I nostri prodotti</h2>
+            <div class="card">
+                <div class="content-img-card">
+
+                </div>
+
+            </div>
+        </div>
+    </main>
     
 </body>
 </html>
