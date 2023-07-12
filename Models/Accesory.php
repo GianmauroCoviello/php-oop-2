04b7f360dfe1223e6bba87dc314c98c8cf3122dd
GianmauroCoviello <user.mail like: "gianmaurocoviello@gmail.com>
2023-07-12 03:19:15 +0200

    [DOCS] stampati a schermo le card dei primi prodotti

--- Models/Accesory.php
+++ Models/Accesory.php
@@ -3,6 +3,22 @@
     // inseriamo le sottoclassi di product richiamandole tramite la proprietà exstends
     class Accesory extends Products{
 
+        public $material;
+        public $size;
+
+
+        public function __construct(string $name, string $image, float $price , String $type, string $material, Float $size) {
+            // $this->var = $var;
+            
+            // ora ci richiamiamo il construct della classe padre
+            parent:: __construct($name, $image, $price, $type, $material, $size);
+
+            $this->material = $material;
+            $this->size = $size;
+           
+
+            
+        }
         
            
 
--- index.php
+++ index.php
@@ -3,18 +3,28 @@
     require_once __DIR__ .'/Models/Products.php';
     require_once __DIR__ .'/Models/Food.php';
     require_once __DIR__ .'/Models/Toy.php';
+    require_once __DIR__ .'/Models/Accesory.php';
 
     // dichiaro una variabile che conterràil contenuto della classe con le instanze
-    $product = new Products('Prodotti', 'https........', 25.99,'Cane');
 
-    $Foods = new Food('food', 'https....', 12.99, 'Gatto', 2, 'carne, ossa di pollo');
+    //  $product = new Products('Prodotti', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 25.99,'Cane');
 
-    $Toys = new Toy('toy', 'https....', 15.99, 'Gatto', +12,);
+    // $Foods = new Food('food', 'https....', 12.99, 'Gatto', 2, 'carne, ossa di pollo');
+
+    // $Toys = new Toy('toy', 'https....', 15.99, 'Gatto', +12,);
+
+    // $Accesorys = new Accesory('Accesory', 'https .....', 35.99,'uccelli', 'plastic', 23);
 
 
-    var_dump($product);
-    var_dump($Foods);
-    var_dump($Toys)
+    // var_dump($product);
+    // var_dump($Foods);
+    // var_dump($Toys);
+    // var_dump($Accesorys)
+    $productsCard = [
+        new Food('Royal Canin Mini Adult','https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg',43.99,'Cane',545,'prosciutto, riso'),
+        new Food('Almo Nature','https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg',44.99,'Cane',600,'manzo, cereali'),
+        new Food('Almo Nature Cat','https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg',34.99,'Gatto',400,'tonno,pollo e prosciutto'),
+    ]
    
 
 ?>
@@ -36,13 +46,23 @@
     <main>
         
         <div class="row">
-            <h2>I nostri prodotti</h2>
-            <div class="card">
-                <div class="content-img-card">
+            <!-- <h2>I nostri prodotti</h2> -->
+            <?php foreach ($productsCard as $product): ?>
+
+                <div class="card">
+                    <div class="content-img-card">
+
+                        <img src="<?php echo $product->image;?>" alt="">
+
+                    </div>
+                    <h2><?php echo $product->name;?></h2>
+                    <p><?php echo $product->type;?></p>
+                    <p>prezzo: <?php echo $product->price;?></p>
+                    <p>peso netto: <?php echo $product->weight;?></p>
+                    <p>Ingredienti: <?php echo $product->ingredients;?></p>
 
                 </div>
-
-            </div>
+            <?php endforeach; ?>    
         </div>
     </main>
     
